6a14075af34b0abdcfe1adf2c54a765281c43916
Jonathan Leczner <jonathanleczner@Jonathan-ZCW-16.local>
2016-09-10 17:02:57 -0400

    initial commit

---
+++ src/main/java/leczner/jon/InitializationLab1/InitializationLab1.java
@@ -0,0 +1,10 @@
+package leczner.jon.InitializationLab1;
+
+/**
+ * Created by jonathanleczner on 9/10/16.
+ */
+public class InitializationLab1 {
+    public static void main(String[] args) {
+        
+    }
+}
